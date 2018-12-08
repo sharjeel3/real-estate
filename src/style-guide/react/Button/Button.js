103fddee7a791d1fc778b1a353cd303dea41bf2a
loupsharjeel <sharjeel@weareloup.com>
2018-12-08 17:56:38 +1100

    Add button component

---
+++ src/style-guide/react/Button/Button.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styles from './Button.module.scss'
+import classnames from 'classnames'
+
+const Button = ({ ...props }) => {
+    const { className, children, ...restProps } = props
+
+    return (
+        <button {...restProps} className={classnames(styles.root, 'btn', {
+            [className]: className
+        })}>
+            {children}
+        </button>
+    )
+}
+
+export default Button
